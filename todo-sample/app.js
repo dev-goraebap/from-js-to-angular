e80470a2c7b24e67db101923b6a6a29a17e707eb
goraebap <dev.goraebap@gmail.com>
2024-04-04 22:34:27 +0900

    refactor: es6 module 시스템으로 todo 파일 분리

--- todo-sample/app.js
+++ todo-sample/app.js
@@ -1,36 +1,4 @@
-let todoList = [];
-
-const addTodo = (content) => {
-    if (!content) {
-        window.alert('내용을 입력해 주세요.');
-        return;
-    }
-
-    const randomId = new Date().getTime().toString();
-
-    todoList.push({
-        id: randomId,
-        content,
-        finished: false,
-    });
-};
-
-const checkTodo = (id) => {
-    todoList = todoList.map((todo) => {
-        if (todo.id !== id) {
-            return todo;
-        }
-        return { ...todo, finished: !todo.finished };
-    });
-
-    return todoList;
-}
-
-const removeTodo = (id) => {
-    todoList = todoList.filter((todo) => todo.id !== id);
-
-    return todoList;
-} 
+import * as todoService from './todo.js';
 
 const formElement = document.querySelector('#todoForm');
 const inputElement = document.querySelector('#contentInput');
@@ -38,32 +6,31 @@ const containerElement = document.querySelector('#todoContainer');
 
 formElement.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    addTodo(inputElement.value);
+    console.log(inputElement.value);
+    todoService.addTodo(inputElement.value);
     inputElement.value = '';
 
     updateUI();
 });
 
 containerElement.addEventListener('click', (event) => {
-    let todoId;
-
     switch (event.target.id) {
         case 'checkTodo':
-            todoId = event.target.getAttribute('data-id');
-            checkTodo(todoId);
+            const todoId2 = event.target.getAttribute('data-id');
+            todoService.checkTodo(todoId2);
             break;
         case 'removeTodo':
-            todoId = event.target.getAttribute('data-id');
-            removeTodo(todoId);
+            const todoId1 = event.target.getAttribute('data-id');
+            todoService.removeTodo(todoId1);
             break;
         default:
     }
 
-    updateUI(); // 모든 처리 후 UI 업데이트
+    updateUI();
 });
 
 const updateUI = () => {
+    const todoList = todoService.getTodoList();
     containerElement.innerHTML = todoList.map((todo) => {
         const contentTag = todo.finished ? 'del' : 'span';
         return `
